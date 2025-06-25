bdae5572f1f556c2af29cc6ee6a84f274cc5bb07
somewhereinsaltwater <mtomorug@gmail.com>
2025-06-25 07:55:08 -1000

    Delete global-plastics-map directory

--- global-plastics-map/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import GlobalPlasticsMap from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GlobalPlasticsMap />
-  </React.StrictMode>
-);
