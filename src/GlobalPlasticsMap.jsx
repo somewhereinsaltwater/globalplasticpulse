b799812054e4b40212ca8e025fff1d24fb49bdaa
somewhereinsaltwater <mtomorug@gmail.com>
2025-06-26 10:01:36 -1000

    Add files via upload

--- src/GlobalPlasticsMap.jsx
+++ src/GlobalPlasticsMap.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import './GlobalPlasticsMap.css';
-import 'mapbox-gl/dist/mapbox-gl.css'; // Ensure mapbox styles are included
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
@@ -44,22 +43,23 @@ const GlobalPlasticsMap = () => {
     }
 
     if (map.current && locations.length) {
-      const markers = document.getElementsByClassName('mapboxgl-marker');
-      while (markers[0]) {
-        markers[0].parentNode.removeChild(markers[0]);
-      }
+      map.current.eachLayer((layer) => {
+        if (layer.id !== 'background' && map.current.getLayer(layer.id)) {
+          map.current.removeLayer(layer.id);
+        }
+      });
 
       locations
         .filter(loc => selectedType === 'All' || loc.type === selectedType)
         .forEach((location) => {
-          const popup = new mapboxgl.Popup({ offset: 25, closeButton: false }).setHTML(\`
+          const popup = new mapboxgl.Popup({ offset: 25, closeButton: false }).setHTML(`
             <div class="popup-box">
-              <strong>\${location.country} – \${location.location}</strong><br />
-              <em>\${location.type}</em><br />
-              <p>\${location.description}</p>
-              <a href="\${location.source}" target="_blank" rel="noopener noreferrer">View Source</a>
+              <strong>${location.country} – ${location.location}</strong><br />
+              <em>${location.type}</em><br />
+              <p>${location.description}</p>
+              <a href="${location.source}" target="_blank" rel="noopener noreferrer">View Source</a>
             </div>
-          \`);
+          `);
           new mapboxgl.Marker({ color: 'black' })
             .setLngLat([location.lng, location.lat])
             .setPopup(popup)
